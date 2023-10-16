b17fba40ec86b456c18db560b53fd773e60a688f
felipem.medeiros <medeirosfe83@gmail.com>
2023-10-16 15:32:03 -0300

    Criando Projeto controle de fluxo

---
+++ src/main/java/controle_fluxo/Main.java
@@ -0,0 +1,21 @@
+package controle_fluxo;
+
+import java.util.Scanner;
+
+import static controle_fluxo.Contador.contar;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
